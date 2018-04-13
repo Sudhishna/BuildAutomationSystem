bfc03cbde3ee49da11779accabf2b9e184baf664
Lakshmi Rajan <lara29.er@gmail.com>
2018-04-13 14:48:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,8 +39,8 @@ def line_prepender(filename, line):
     os.system('echo %s|sudo -S %s' % (PW, command))
     
 def populate_ips(devInfo="Info.txt"):
-    with open(devInfo, 'a') as f:
-        f.write(hostip)
+    with open(devInfo, 'w') as f:
+        f.write(hostip +'\n')
         f.write(fileserver)
         
 print("\n\n      ########  Making initial Installations  ########")
